b08a002727958567a350e9abf9bb243c350d482d
Kalebtes2031 <kalebtesfaye2031@gmail.com>
2025-10-16 15:27:10 +0300

    refactor(layout): update root layouts for consistency

--- realstateapp/app/(root)/_layout.tsx
+++ realstateapp/app/(root)/_layout.tsx
@@ -1,6 +1,12 @@
+import { useGlobalContext } from "@/contexts/GlobalProvider";
 import { Redirect, Slot } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AppLayout(){
+    const { loading, isLogged} = useGlobalContext();
+
+    if(!isLogged){
+        return <Redirect href='/signin' />
+    }
     return <Slot />;
 }
